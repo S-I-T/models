c1b07f54df7cff4d95191d5d899dde6697452d00
pabgonza <pabgonza@gmail.com>
2017-11-09 15:57:13 -0300

    bug

--- research/slim/datasets/trucks.py
+++ research/slim/datasets/trucks.py
@@ -101,7 +101,7 @@ def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
 	
 	if not file_pattern:
 		file_pattern = _FILE_PATTERN
-	file_pattern = os.path.join(dataset_dir + '/tfrecord', file_pattern % split_name)
+	file_pattern = os.path.join(dataset_dir + '/tfrecords', file_pattern % split_name)
 	
 	# Allowing None in the signature so that dataset_factory can use the default.
 	if reader is None:
